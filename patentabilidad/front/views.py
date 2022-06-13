fc29968337df7214904d26eeba58ef4a0b5ef136
JuanSaldana <jasaldanah@gmail.com>
2022-06-13 18:35:15 -0500

    feat: index and login page added

--- patentabilidad/front/views.py
+++ patentabilidad/front/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, "front/index.html")
+
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        print(username)
+        password = request.POST["password"]
+        print(password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "front/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "front/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
