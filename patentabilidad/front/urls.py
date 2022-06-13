596c4058a08bed4cdeb47362e558bd2a8393c77b
JuanSaldana <jasaldanah@gmail.com>
2022-06-13 18:34:55 -0500

    config: added User model

---
+++ patentabilidad/front/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    
+    # authenticate views
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
+]
