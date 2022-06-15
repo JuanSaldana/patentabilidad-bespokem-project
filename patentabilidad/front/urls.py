3080eca36684f2a0e5e38d38daf7df541eb3b3e8
JuanSaldana <jasaldanah@gmail.com>
2022-06-15 11:36:43 -0500

    feat: favicon added

--- patentabilidad/front/urls.py
+++ patentabilidad/front/urls.py
@@ -1,8 +1,11 @@
 ﻿from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('front/icons/favicon.ico')), name="favicon.ico"),
     
     # authenticate views
     path("login", views.login_view, name="login"),
